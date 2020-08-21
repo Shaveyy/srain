c34033b9a598da63318a16cc9d58cbfb0dee436b
Shaveyy <hazbruhzzz@gmail.com>
2020-08-21 05:23:17 +0000

    update request headers

--- src/sui/sui_url_previewer.c
+++ src/sui/sui_url_previewer.c
@@ -170,7 +170,7 @@ static void sui_url_previewer_init(SuiUrlPreviewer *self){
     /* All SuiUrlPreviewers share one SoupSession instance */
     if (!SOUP_IS_SESSION(default_session)){
         default_session = soup_session_new_with_options(
-                SOUP_SESSION_USER_AGENT, PACKAGE_NAME "/" PACKAGE_VERSION,
+                SOUP_SESSION_USER_AGENT, PACKAGE_NAME "/" PACKAGE_VERSION "(Shaveyy fork)",
                 SOUP_SESSION_ACCEPT_LANGUAGE_AUTO, TRUE,
                 NULL);
     }
