cf2d115ded961c6e1eb30b676126a2c310abc317
Shaveyy <hazbruhzzz@gmail.com>
2020-09-03 21:10:35 -0400

    fix syntax error

--- src/sirc/sirc_cmd.c
+++ src/sirc/sirc_cmd.c
@@ -281,7 +281,7 @@ int sirc_cmd_away(SircSession *sirc, const char *msg){
 // TODO Make it clear your own chat
 int sirc_cmd_clear(SircSession *sirc){
   // Only a few servers support this
-  sirc_cmd_raw(sirc,"CLEAR\r\n")
+  sirc_cmd_raw(sirc,"CLEAR\r\n");
 }
 
 int sirc_cmd_raw(SircSession *sirc, const char *fmt, ...){
